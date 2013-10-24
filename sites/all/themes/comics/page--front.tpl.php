d220c40b6fd38210eeb3b040fd1d6174a057f732
max <max@example.com>
2013-10-24 10:16:10 +0800

    this is vison 1.0.1

--- sites/all/themes/comics/page--front.tpl.php
+++ sites/all/themes/comics/page--front.tpl.php
@@ -1,15 +1,21 @@
 <div id="page_wrapper"><!-- start page_wrapper -->
 
     <div class="page_top">
-          
-
+      <div class="main"> <?php print render($page['pagetop']) ; ?>  </div>        
     </div>
     <div id="header">
+     <div class="logo">
       <?php if ($logo): ?>
             <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
-            <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
+            <img src="<?php print $logo; ?>" alt="<?php print t('这是logo'); ?>" />
             </a>
          <?php endif; ?>
+      </div>
+      <div class="header_main">    
+      <?php  if($page['header']): ?>
+        <?php  print render($page['header']) ; ?>
+      <?php endif ?>
+      </div>
     </div> <!-- header end -->
 
     <div id="menu">
@@ -31,11 +37,13 @@
       <?php endif; ?>
     </div> <!--main menu end -->
     <div id="center">
-
+      <div class="center_main">
+        <?php  print render($page['home']);?>
+      </div>
     </div> <!-- center --->
 
     <div id="footer">
-
+     <div class="footer_main"> <?php print render($page['footer']); ?> </div>
     </div> <!-- footer end -->
 
 </div><!-- end page_wrapper-->
